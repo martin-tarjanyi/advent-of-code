385f53b95c1239666c6388bfdb95df28e5b57a24
Martin Tarjányi <17810336+martin-tarjanyi@users.noreply.github.com>
2025-12-06 15:10:13 +0100

    2025 Day 5 - part 2

--- src/main/kotlin/com/example/aoc/y2025/Day05.kt
+++ src/main/kotlin/com/example/aoc/y2025/Day05.kt
@@ -3,8 +3,8 @@ package com.example.aoc.y2025
 import com.example.aoc.inputLineSequence
 
 fun main() {
-    part1()
-//    part2()
+//    part1()
+    part2()
 }
 
 private fun part1() {
@@ -23,4 +23,31 @@ private fun part1() {
 
     ids.count { id -> freshRanges.any { id in it } }
         .also { println(it) }
+}
+
+private fun part2() {
+    val input = inputLineSequence("2025/day05.txt").toList()
+
+    val freshRanges =
+        input.takeWhile { it.contains("-") }
+            .map {
+                val (start, end) = it.split("-")
+                start.toLong()..end.toLong()
+            }
+            .sortedBy { it.first }
+
+    val newRanges = mutableListOf<LongRange>().apply {
+        add(freshRanges.first())
+    }
+
+    freshRanges.drop(1).forEachIndexed { index, range ->
+        val lastRange = newRanges.last()
+        if (range.first in lastRange) {
+            newRanges[newRanges.size - 1] = lastRange.first..maxOf(lastRange.last, range.last)
+        } else {
+            newRanges.add(range)
+        }
+    }
+
+    println(newRanges.sumOf { it.last - it.first + 1 })
 }
