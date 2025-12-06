a68f6352c2ea3fd4db39bed37f28c1e18064d403
Martin Tarjányi <17810336+martin-tarjanyi@users.noreply.github.com>
2025-12-06 09:49:52 +0100

    2025 Day 1 - part 2

--- src/main/kotlin/com/example/aoc/y2025/Day01.kt
+++ src/main/kotlin/com/example/aoc/y2025/Day01.kt
@@ -1,6 +1,8 @@
 package com.example.aoc.y2025
 
 import com.example.aoc.inputLineSequence
+import kotlin.math.abs
+import kotlin.math.max
 
 fun main() {
     val dirAndMove = inputLineSequence("2025/day01.txt")
@@ -10,6 +12,7 @@ fun main() {
         .toList()
 
     part1(dirAndMove)
+    part2(dirAndMove)
 }
 
 private fun part1(dirAndMove: List<Pair<Char, Long>>) {
@@ -17,7 +20,6 @@ private fun part1(dirAndMove: List<Pair<Char, Long>>) {
     var currentPosition: Long = 50
 
     dirAndMove.forEach { (dir, move) ->
-        println("$dir$move")
         when (dir) {
             'R' -> currentPosition += move
             'L' -> currentPosition -= move
@@ -27,6 +29,34 @@ private fun part1(dirAndMove: List<Pair<Char, Long>>) {
         if (currentPosition % 100 == 0L) {
             numberOfZeroPositions++
         }
+    }
+
+    println(numberOfZeroPositions)
+}
+
+private fun part2(dirAndMove: List<Pair<Char, Long>>) {
+    var numberOfZeroPositions: Long = 0
+    var currentPosition: Long = 50
+
+    dirAndMove.forEach { (dir, move) ->
+        val previousPosition = currentPosition
+        when (dir) {
+            'R' -> currentPosition += move
+            'L' -> currentPosition -= move
+            else -> error("Unexpected dir: $dir")
+        }
+        val offset = max(abs(currentPosition), abs(previousPosition)) * 100
+
+        val normalizedPrevious = offset + previousPosition
+        val normalizedCurrent = offset + currentPosition
+        var zeroPositions = abs(normalizedPrevious / 100 - normalizedCurrent / 100)
+        if (previousPosition % 100 == 0L && previousPosition > currentPosition) {
+            zeroPositions--
+        }
+        if (currentPosition % 100 == 0L && previousPosition > currentPosition) {
+            zeroPositions++
+        }
+        numberOfZeroPositions += zeroPositions
     }
 
     println(numberOfZeroPositions)
