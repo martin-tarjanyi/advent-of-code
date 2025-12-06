e6b6f25dc3642613e913c15ac05363f3b5da5259
Martin Tarjányi <17810336+martin-tarjanyi@users.noreply.github.com>
2025-12-06 14:39:14 +0100

    2025 Day 4 - part 2

--- src/main/kotlin/com/example/aoc/y2025/Day04.kt
+++ src/main/kotlin/com/example/aoc/y2025/Day04.kt
@@ -3,20 +3,21 @@ package com.example.aoc.y2025
 import com.example.aoc.inputLineSequence
 
 fun main() {
-    part1()
+//    part1()
+    part2()
 }
 
 private fun part1() {
     val grid = inputLineSequence("2025/day04.txt")
-        .map { it.split("") }
+        .map { it.toMutableList() }
         .toList()
 
     grid.mapIndexed { rowIndex, row ->
         row.filterIndexed { cellIndex, cell ->
-            if (cell == "@") {
+            if (cell == '@') {
                 val adjacentPapers = ((rowIndex - 1)..(rowIndex + 1))
                     .flatMap { i -> ((cellIndex - 1)..(cellIndex + 1)).map { i to it } }
-                    .count { (i, j) -> grid.getOrNull(i)?.getOrNull(j) == "@" } - 1 // subtract self
+                    .count { (i, j) -> grid.getOrNull(i)?.getOrNull(j) == '@' } - 1 // subtract self
                 adjacentPapers < 4
             } else {
                 false
@@ -24,4 +25,51 @@ private fun part1() {
         }.count()
     }.sum()
         .let { println(it) }
+}
+
+
+private fun part2() {
+    val currentGrid = inputLineSequence("2025/day04.txt")
+        .map { it.toMutableList() }
+        .toMutableList()
+
+    var removablePaperCount = 0
+
+    while (true) {
+        val removablePapers = currentGrid.flatMapIndexed { rowIndex, row ->
+            row.mapIndexedNotNull { cellIndex, cell ->
+                if (cell == '.') {
+                    return@mapIndexedNotNull null
+                }
+
+                val adjacentPapers = ((rowIndex - 1)..(rowIndex + 1))
+                    .flatMap { i -> ((cellIndex - 1)..(cellIndex + 1)).map { i to it } }
+                    .count { (i, j) -> currentGrid.getOrNull(i)?.getOrNull(j) == '@' } - 1 // subtract self
+
+                if (adjacentPapers < 4) {
+                    return@mapIndexedNotNull rowIndex to cellIndex
+                }
+
+                return@mapIndexedNotNull null
+            }
+        }
+
+        if (removablePapers.isEmpty()) {
+            break
+        }
+
+        removablePaperCount += removablePapers.size
+        removablePapers.forEach { (i, j) ->
+            currentGrid[i][j] = '.'
+        }
+
+//        println("Removed papers: ${removablePapers.size}")
+//        println("Grid:")
+//        currentGrid.forEach { row ->
+//            println(row.joinToString(separator = ""))
+//        }
+//        println()
+    }
+
+    println(removablePaperCount)
 }
