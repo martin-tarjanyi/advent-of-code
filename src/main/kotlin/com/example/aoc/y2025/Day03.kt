bfda11ffe1ec604b9d165069eb6de3c76c855eef
Martin Tarjányi <17810336+martin-tarjanyi@users.noreply.github.com>
2025-12-06 13:49:30 +0100

    2025 Day 3 - part 2

--- src/main/kotlin/com/example/aoc/y2025/Day03.kt
+++ src/main/kotlin/com/example/aoc/y2025/Day03.kt
@@ -1,8 +1,14 @@
 package com.example.aoc.y2025
 
 import com.example.aoc.inputLineSequence
+import kotlin.text.indexOf
 
 fun main() {
+//    part1()
+    part2()
+}
+
+private fun part1() {
     inputLineSequence("2025/day03.txt")
         .map {
             val nums = it.map { it.digitToInt() }
@@ -13,4 +19,23 @@ fun main() {
         }
         .sum()
         .let { println(it) }
+}
+
+private fun part2() {
+    inputLineSequence("2025/day03.txt")
+        .map { largestNumber(it).toLong() }
+        .sum()
+        .let { println(it) }
+}
+
+fun largestNumber(num: String, digits: Int = 12): String {
+    if (num.isEmpty() || digits == 0) {
+        return ""
+    }
+
+    val maxIndex = num.length - digits
+    val bestDigit = num.take(maxIndex + 1).map { it.digitToInt() }.max()
+    val bestIndex = num.indexOf(bestDigit.digitToChar())
+
+    return bestDigit.digitToChar() + largestNumber(num.drop(bestIndex + 1), digits = digits - 1)
 }
