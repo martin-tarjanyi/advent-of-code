667077af6efc55184f08b796f1caad76a201942f
Martin Tarjányi <17810336+martin-tarjanyi@users.noreply.github.com>
2025-12-06 10:05:00 +0100

    2025 Day 2 - part 1

---
+++ src/main/kotlin/com/example/aoc/y2025/Day02.kt
@@ -0,0 +1,24 @@
+package com.example.aoc.y2025
+
+import com.example.aoc.inputLineSequence
+
+fun main() {
+    inputLineSequence("2025/day02.txt")
+        .flatMap { it.split(',') }
+        .map { it.split('-').let { it[0].toLong() to it[1].toLong() } }
+        .flatMap { (start, end) -> (start..end)
+            .filter {
+                it.toString().isInvalidPart1()
+            }
+        }
+        .sum()
+        .let { println(it) }
+}
+
+private fun String.isInvalidPart1(): Boolean {
+    if (length % 2 == 1) {
+        return false
+    }
+    val sequenceLength = length / 2
+    return substring(0, sequenceLength) == substring(sequenceLength)
+}
